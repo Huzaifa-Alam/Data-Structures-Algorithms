696e44348793931926f2479c2af4edf260a5c1d6
Huzaifa-Alam <huzaifaalam5584@gmail.com>
2024-09-25 18:09:17 +0500

    Update Insertion_Max-Heap.c

--- Heap/Insertion_Max-Heap.c
+++ Heap/Insertion_Max-Heap.c
@@ -1,6 +1,32 @@
-# include<stdio.h>
- int main(){
-    printf("Hello, Heap!\n");
+#include <stdio.h>
 
-    return 0;
- }
+int insertHeap(array, size, value)
+{
+   int * array = (int*)malloc (size * ); 
+   size += size;
+   array[size] = value;
+   int index = size;
+   while (index > 1)
+   {
+      int parent = index / 2;
+      if (array[size] < array[parent])
+      {
+         swap(array[size], array[parent]);
+         index = parent;
+      }
+      else
+      {
+         return array;
+      }
+   }
+}
+
+int main()
+{
+   printf("Hello, Heap!\n");
+   int value;
+   int array = {70, 50, 40, 45, 35, 39, 16, 10, 9};
+   int size = sizeof(array) / sizeof(array[0]);
+
+   return 0;
+}
