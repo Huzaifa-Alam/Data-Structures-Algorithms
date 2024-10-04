bdbd7ede6ed1b382f32f9a53e4e99d086dd7303a
Huzaifa-Alam <huzaifaalam5584@gmail.com>
2024-10-04 21:02:33 +0500

    Update HeapSort.c

--- Heap/HeapSort.c
+++ Heap/HeapSort.c
@@ -15,7 +15,7 @@ void heapSort(int array[], int n)
         int temp = array[0];
         array[0] = array[size - 1];
         array[size - 1] = temp;
-        size--;
+        size = size - 1;
 
         // Heapify the root element
         maxHeap(array, size, 0); // Use index 0 as the root for heapify
@@ -25,8 +25,8 @@ void heapSort(int array[], int n)
 void maxHeap(int array[], int size, int i)
 {
     int largest = i;       // Initialize largest as root
-    int left = 2 * i + 1;  // Left child index
-    int right = 2 * i + 2; // Right child index
+    int left = (2 * i) + 1;  // Left child index
+    int right = (2 * i) + 2; // Right child index
 
     // Check if left child exists and is greater than root
     if (left < size && array[left] > array[largest])
